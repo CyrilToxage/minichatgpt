459a9c2b494613d195d61c9cbbdd8610f4476072
CyrilToxage <mcyril2106@gmail.com>
2025-03-22 23:52:15 +0100

    1.0.2 - fini

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,5 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
-import Chatgpt from '@/Components/Chatgpt.vue';
 </script>
 
 <template>
@@ -14,7 +13,6 @@ import Chatgpt from '@/Components/Chatgpt.vue';
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Chatgpt />
                 </div>
             </div>
         </div>
